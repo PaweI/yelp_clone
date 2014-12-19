e63d7f7bdd1e9c5a7986dab3b50a28ab4f8c8fc3
Nick Dyer <nbdyer@gmail.com>
2014-12-19 15:15:38 +0000

    Move default image

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,7 +4,7 @@ class Restaurant < ActiveRecord::Base
 
   belongs_to :user
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/missing.jpg"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ":style/missing.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   validates :name, length: {minimum: 3}, uniqueness: true
